ada7ed23bee11370f0ecd3f3dd52f29cd5ca9812
sergcourt <sergiovillani8@gmail.com>
2017-03-15 17:47:15 -0400

    removed blocks

--- additions/TODO_8_ConferenceApi.java
+++
@@ -1,6 +0,0 @@
-
-        final Queue queue = QueueFactory.getDefaultQueue();
-        queue.add(ofy().getTransaction(),
-                TaskOptions.Builder.withUrl("/tasks/send_confirmation_email")
-                .param("email", profile.getMainEmail())
-                .param("conferenceInfo", conference.toString()));
